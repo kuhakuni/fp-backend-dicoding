5fe9b5f69bc3cfd4dd32c0cca6043e810bc955b1
kuhakuni <afif.haidar@student.ub.ac.id>
2024-03-14 22:01:51 +0700

    feat: update version

--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,18 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('../routes/routes');
+const Hapi = require("@hapi/hapi");
+const routes = require("../routes/routes");
 
 const init = async () => {
 	const server = Hapi.server({
 		port: 9000,
-		host: 'localhost',
+		host: "localhost",
 		routes: {
 			cors: {
-				origin: ['*'],
+				origin: ["*"],
 			},
 		},
 	});
 	server.route(routes);
 	await server.start();
-	console.log(`Server berjalan pada port ${server.info.uri}`);
 };
 
 init();
